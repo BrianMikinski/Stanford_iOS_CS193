baa18183ed6bd07753f83ff134cfade4fde95321
Brian Mikinski <brian.mikinski@gmail.com>
2015-03-14 13:24:49 -0500

    Moving business logic out of view model

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,103 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Brian Thomas Mikinski on 3/14/15.
+//  Copyright (c) 2015 Brian Thomas Mikinski. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op: Printable {
+        case Operand(Double)
+        case UnaryOperation(String, Double ->Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _ ):
+                    return "\(symbol)"
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    //Alternative form of displaying array
+    //var opStack = Array<Op>()
+    private var opStack = [Op]()
+    
+    //Alternative form of declaring dictionay
+    //var knownOps = Dictionary<String, Op>()
+    private var knownOps = [String:Op]()
+    
+    init()
+    {
+        knownOps["x"] = Op.BinaryOperation("x", *)
+        knownOps["÷"] = Op.BinaryOperation("÷"){$1 * $0}
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["-"] = Op.BinaryOperation("-"){$0 * $1}
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    //Returning a tuble to hold our evaluation and the remaining operations
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        //There is an error here because it is an array and not a class. That means that the 
+        //operation is passed by value as opposed to reference! So it is copied and not referenced
+        if !ops.isEmpty {
+            
+            //Creating a mutable array of ops. Because ops is really "let ops: [Op]" in the method parameters
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+                
+                // "_" underscore means that we aren't passing anything to the function
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() ->Double? {
+        //Tuple closure? declare a tuple without a type?
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double?{
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        
+        return evaluate()
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,114 +17,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var lblHistory: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber = false
-    var floatingPointNumberHasBeenInput = false
-    var resetHistory = false
-    var constantPresent = false
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var constantPresent: Bool = false
     
-    var operandStack = Array<Double>()
+    var brain = CalculatorBrain()
     
     //Append a digit to the top of the stack
     @IBAction func appendDigit(sender: UIButton) {
-        var digit = sender.currentTitle!
         
-        if((display.text!.rangeOfString(".") == nil) || (digit != ".") || (display.text! == "")) {
-            
-            if userIsInTheMiddleOfTypingANumber {
-
-                if isConstant(display.text!)
-                {
-                    display.text = "\(displayValue * NSNumberFormatter().numberFromString(digit)!.doubleValue)"
-                }
-                else
-                {
-                
-                    switch digit {
-                
-                        case "π": display.text = "\(displayValue * M_PI)"
-                        
-                        default: display.text = display.text! + digit
-                    }
-                }
-            }
-            else {
-                
-                display.text = digit
-                userIsInTheMiddleOfTypingANumber = true
-            }
-            
-            appendHistory(digit)
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
         }
     }
     
-    //Reset the app to its orginal state
-    @IBAction func clearAll(sender: UIButton) {
-        operandStack.removeAll(keepCapacity: false)
-        lblHistory.text = ""
-        display.text = "0"
-        userIsInTheMiddleOfTypingANumber = false;
-        println("operandStack = \(operandStack)")
+    //Perform a mathematical operation
+    @IBAction func operate(sender: UIButton) {
         
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
     }
     
     //Add the key to the top of the stack
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
-        floatingPointNumberHasBeenInput  = false
-        constantPresent = false
-        
-        operandStack.append(displayValue)
-        
-        
-        //operandStack.append(displayValue)
-        println("operandStack = \(operandStack)")
-    }
-    
-    //Perform a mathematical operation
-    @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
-        
-        if !userIsInTheMiddleOfTypingANumber {
-            
-            appendHistory(operation)
-            appendEquals()
-        
-            switch operation {
-                
-            case "×": performOperation { $0 * $1 }
-            case "÷": performOperation { $0 / $1 }
-            case "+": performOperation { $0 + $1 }
-            case "−": performOperation { $0 - $1 }
-            case "√": performOperation { sqrt($0) }
-            case "sin": performOperation { sin($0) }
-            case "cos": performOperation { cos($0) }
-            default: break
-                
-                //          You can do this if you need want to be more verbose
-                //        case "×": performOperation({(op1: Double, op2: Double) -> Double in
-                //            return op1  * op2
-                
-            }
-            
-            resetHistory = true
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
         }
-    }
-    
-    //Add to the history label text box
-    func appendHistory(newString: String ) {
-        lblHistory.text = lblHistory.text! + newString
-    }
-    
-    func appendEquals()
-    {
-        lblHistory.text = lblHistory.text! + "="
-    }
-    
-    func isConstant(newString: String) -> Bool
-    {
-        switch newString {
-        case "π": return true
-        default: return false
+        else {
+            displayValue = 0
         }
     }
     
@@ -146,22 +79,32 @@ class ViewController: UIViewController {
         }
     }
     
-    //Execute a function that takes two doubles and outputs
-    //another double
-    func performOperation(operation: (Double, Double) -> Double ) {
-        if operandStack.count >= 2{
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast()  )
-            enter()
+    //Reset the app to its orginal state
+    @IBAction func clearAll(sender: UIButton) {
+        //operandStack.removeAll(keepCapacity: false)
+        lblHistory.text = ""
+        display.text = "0"
+        userIsInTheMiddleOfTypingANumber = false;
+    }
+    
+    //Add to the history label text box
+    func appendHistory(newString: String ) {
+        lblHistory.text = lblHistory.text! + newString
+    }
+    
+    func appendEquals()
+    {
+        lblHistory.text = lblHistory.text! + "="
+    }
+    
+    func isConstant(newString: String) -> Bool
+    {
+        switch newString {
+        case "π": return true
+        default: return false
         }
     }
     
-    //Execute a function that takes one double and outputs
-    //a double
-    func performOperation(operation: (Double) -> Double ) {
-        if operandStack.count >= 2{
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
+   
 }
 
